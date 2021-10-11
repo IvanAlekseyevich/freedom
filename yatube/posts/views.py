fdf5f91e8008ba414cde31450710afb24fdfcf00
pozarnik <pozarnik@tut.by>
2021-10-11 04:59:46 +0300

    Before sleep

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context) 
 
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts(request, slug):
-    return HttpResponse(f'Группировка {slug}')
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context) 
