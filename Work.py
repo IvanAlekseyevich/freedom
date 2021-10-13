56971f9aa3ee6e99d9648ef177d3c0cf96fa9581
pozarnik <pozarnik@tut.by>
2021-10-13 14:38:09 +0300

    1

---
+++ Work.py
@@ -0,0 +1,6 @@
+#source venv/Scripts/activate
+#cd yatube
+#python manage.py runserver
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py createsuperuser
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-<<<<<<< HEAD
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -12,16 +11,4 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
-    )
-    group = models.TextField(null=True, blank=True)
-
-
-class Group(models.Model):
-    title = models.TextField()
-    slug = models.TextField()
-    description = models.TextField()
-    def __str__(self):
-        return self.title
-=======
-# Create your models here.
->>>>>>> parent of 5487f2c (Added admin and post (test))
+    ) 
