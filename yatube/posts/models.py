5487f2cb3e3b019a181662a75ecb77304f6fe6a0
pozarnik <pozarnik@tut.by>
2021-10-13 13:01:43 +0300

    Added admin and post (test)

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.TextField()
+    slug = 
+    description = models.TextField()
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = Group(blank=True, null=True)
+
+
+# class Event(models.Model):
+#     name = models.CharField(max_length=200)
+#     start_at = models.DateTimeField(auto_now_add=True)
+#     description = models.TextField() 
+#     contact = models.EmailField()
+#     author = models.ForeignKey(
+#         User, 
+#         on_delete=models.CASCADE, 
+#         related_name="events"
+#     )
+#     location = models.CharField(max_length=400)
