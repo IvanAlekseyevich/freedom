7773aacbb9508f053cbdf7c194ecf43ddb2381fa
pozarnik <pozarnik@tut.by>
2021-10-31 15:20:50 +0300

    Update views.py
    Добавлен paginator, изменен вывод объектов на страницу

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,23 +1,41 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-
 from .models import Post, Group
 
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    post_list = Post.objects.all().order_by('-pub_date')
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядить так:
+    # post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10) 
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
+    # posts = Post.objects.order_by('-pub_date')[:10]
+    # context = {
+    #     'posts': posts,
+    # }
     return render(request, template, context) 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.order_by('-pub_date')[:10]
+    post_list = group.posts.order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context) 
     
