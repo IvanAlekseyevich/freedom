10ad960eb32e842b004453ff5596b206daa5137b
pozarnik <pozarnik@tut.by>
2021-11-02 20:20:01 +0300

    Добавлен декоратор

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,20 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Group
+
+
+def authorized_only(func):
+    # Функция-обёртка в декораторе может быть названа как угодно
+    def check_user(request, *args, **kwargs):
+        # В любую view-функции первым аргументом передаётся объект request,
+        # в котором есть булева переменная is_authenticated,
+        # определяющая, авторизован ли пользователь.
+        if request.user.is_authenticated:
+            # Возвращает view-функцию, если пользователь авторизован.
+            return func(request, *args, **kwargs)
+        # Если пользователь не авторизован — отправим его на страницу логина.
+        return redirect('/auth/login/')        
+    return check_user
 
 
 def index(request):
