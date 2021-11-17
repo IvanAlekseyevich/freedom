479cf7a8e5fe8b5ef7f75848a0b19842f78f5420
pozarnik <pozarnik@tut.by>
2021-11-17 21:05:04 +0300

    Добавлен url создания поста

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('profile/<str:username>/', views.profile, name='profile'),
     # Просмотр записи
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    # Создание записи
+    path('create/', views.post_create, name='post_create'),
 ]
