2ece2e9d8811c44cecce19fd4c5fef0e7b43523f
pozarnik <pozarnik@tut.by>
2021-11-17 21:04:44 +0300

    Добавлена проверка формы

---
+++ yatube/posts/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Post
+
+
+#  создадим собственный класс для формы регистрации
+#  сделаем его наследником предустановленного класса UserCreationForm
+class PostForm(forms.ModelForm):
+    class Meta:
+        # укажем модель, с которой связана создаваемая форма
+        model = Post
+        # укажем, какие поля должны быть видны в форме и в каком порядке
+        fields = ('text', 'group')
+
+    def clean_subject(self):
+        data = self.cleaned_data['text']
+
+        # Если пользователь не поблагодарил администратора - считаем это ошибкой
+        if data == '':
+            raise forms.ValidationError('Это поле обязательно к заполнению')
+
+        # Метод-валидатор обязательно должен вернуть очищенные данные, 
+        # даже если не изменил их
+        return data 
