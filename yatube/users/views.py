77d34c39987611f34df823a4d8355899d4f2d9cc
pozarnik <pozarnik@tut.by>
2022-01-23 17:53:37 +0300

    Исправление комментария

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -12,6 +12,6 @@ from .forms import CreationForm
 
 class SignUp(CreateView):
     form_class = CreationForm
-    # После успешной регистрации перенаправляем пользователя на главную.
+    # После успешной регистрации перенаправляем пользователя на страницу логина.
     success_url = reverse_lazy('users:login')
     template_name = 'users/signup.html' 
