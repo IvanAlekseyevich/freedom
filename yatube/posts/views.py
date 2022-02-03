b4e9ab863feb8ab7255d1b48fd18ff74eeee2fe5
pozarnik <pozarnik@tut.by>
2022-02-04 02:46:21 +0300

    удаление ненужного импорта

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Group, User
 from .forms import PostForm
 
