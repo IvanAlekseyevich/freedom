65075d5801fd0eb9698e3eb50eaa4c971d89c55a
pozarnik <pozarnik@tut.by>
2022-02-05 00:03:36 +0300

    добавлена пустая строка

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -16,4 +16,3 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/',  PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ] 
-
