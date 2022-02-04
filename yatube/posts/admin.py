ed52410b1655bb67e55678fb7d1d17fe2bd79c25
pozarnik <pozarnik@tut.by>
2022-02-04 22:24:07 +0300

    Изменен стиль написания

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -5,7 +5,7 @@ from .models import Post, Group
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
-        'get_descriptiontext',
+        'get_description_text',
         'pub_date',
         'author',
         'group',
