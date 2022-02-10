43f4e86b0f3df250f61bc1b33a0116ce5ef0fb5d
pozarnik <pozarnik@tut.by>
2022-02-10 20:01:31 +0300

    Update forms.py

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -5,5 +5,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        # укажем, какие поля должны быть видны в форме и в каком порядке
         fields = ('text', 'group')
