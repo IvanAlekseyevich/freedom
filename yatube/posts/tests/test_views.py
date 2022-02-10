2460fadec2ea32e897bf7dcf3bcf642f84f45bdf
pozarnik <pozarnik@tut.by>
2022-02-10 16:23:53 +0300

    Оптимизация

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -21,22 +21,16 @@ class PostURLTests(TestCase):
             slug='testslug',
             description='Тестовое описание'
         )
-        cls.test_post = Post.objects.create(
+        for num in range(12):
+            f'test_post_{num}' == Post.objects.create(
             text='Тестовый пост',
-            pub_date='1854-03-14',
+            pub_date='1854-03-14 00:00:00',
             author= cls.user,
             group = cls.test_group
         )
-        for num in range(11):
-            num == Post.objects.create(
+        cls.test_post_13 = Post.objects.create(
             text='Тестовый пост',
-            pub_date='1854-03-14',
-            author= cls.user,
-            group = cls.test_group
-        )
-        cls.test_post12 = Post.objects.create(
-            text='Тестовый пост',
-            pub_date='1854-03-26',
+            pub_date='1854-03-15 00:00:00',
             author= cls.user
         )
 
@@ -56,7 +50,7 @@ class PostURLTests(TestCase):
             reverse('posts:index'),
             reverse('posts:group_list', kwargs={'slug': PostURLTests.test_group.slug}),
             reverse('posts:profile', kwargs={'username': PostURLTests.user}),
-            reverse('posts:post_detail', kwargs={'post_id': PostURLTests.test_post.id})
+            reverse('posts:post_detail', kwargs={'post_id': PostURLTests.test_post_13.id})
         )
         for reverse_name in PAGES:
             with self.subTest(reverse_name=reverse_name):
@@ -70,7 +64,7 @@ class PostURLTests(TestCase):
 
     def test_post_edit_url_exists_at_author(self):
         """Страница /edit/ доступна автору поста."""
-        response = self.authorized_client.post(reverse('posts:post_edit', kwargs={'post_id': PostURLTests.test_post.id}))
+        response = self.authorized_client.post(reverse('posts:post_edit', kwargs={'post_id': PostURLTests.test_post_13.id}))
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
     def test_post_url_redirect_anonymous(self):
@@ -89,8 +83,8 @@ class PostURLTests(TestCase):
             reverse('posts:index'): 'posts/index.html',
             reverse('posts:group_list', kwargs={'slug': PostURLTests.test_group.slug}): 'posts/group_list.html',
             reverse('posts:profile', kwargs={'username': PostURLTests.user}): 'posts/profile.html',
-            reverse('posts:post_detail', kwargs={'post_id': PostURLTests.test_post.id}): 'posts/post_detail.html',
-            reverse('posts:post_edit', kwargs={'post_id': PostURLTests.test_post.id}): 'posts/create_post.html',
+            reverse('posts:post_detail', kwargs={'post_id': PostURLTests.test_post_13.id}): 'posts/post_detail.html',
+            reverse('posts:post_edit', kwargs={'post_id': PostURLTests.test_post_13.id}): 'posts/create_post.html',
             reverse('posts:post_create'): 'posts/create_post.html', 
         }
         for reverse_name, template in templates_url_names.items():
@@ -130,8 +124,8 @@ class PostURLTests(TestCase):
         post_text_0 = first_object.text
         post_pub_date_0 = first_object.pub_date
         post_author_0 = first_object.author
-        self.assertEqual(post_text_0, PostURLTests.test_post12.text)
-        self.assertEqual(post_pub_date_0, PostURLTests.test_post12.pub_date)
+        self.assertEqual(post_text_0, PostURLTests.test_post_13.text)
+        self.assertEqual(post_pub_date_0, PostURLTests.test_post_13.pub_date)
         self.assertEqual(post_author_0, PostURLTests.user)
 
     # Проверяем, что словарь context страницы /
@@ -148,15 +142,15 @@ class PostURLTests(TestCase):
         post_author_0 = first_object.author
         group_title_0 = second_object.title
         post_text_0 = first_object.text
-        self.assertEqual(post_text_0, PostURLTests.test_post12.text)
-        self.assertEqual(post_pub_date_0, PostURLTests.test_post12.pub_date)
+        self.assertEqual(post_text_0, PostURLTests.test_post_13.text)
+        self.assertEqual(post_pub_date_0, PostURLTests.test_post_13.pub_date)
         self.assertEqual(post_author_0, PostURLTests.user)
         self.assertEqual(group_description_0, PostURLTests.test_group.description)
         self.assertEqual(group_title_0, PostURLTests.test_group.title)
 
     def test_post_detail_page_show_correct_context(self):
         """Шаблон post_detail сформирован с правильным контекстом."""
-        response = self.guest_client.get(reverse('posts:post_detail', kwargs={'post_id': PostURLTests.test_post.id}))
+        response = self.guest_client.get(reverse('posts:post_detail', kwargs={'post_id': PostURLTests.test_post_13.id}))
         # Взяли первый элемент из списка и проверили, что его содержание
         # совпадает с ожидаемым
         first_object = response.context['post_detail']
@@ -165,10 +159,10 @@ class PostURLTests(TestCase):
         post_pub_date_0 = first_object.pub_date
         post_author_0 = first_object.author
         post_text_0 = first_object.text
-        self.assertEqual(post_text_0, PostURLTests.test_post.text)
-        self.assertEqual(post_pub_date_0, PostURLTests.test_post.pub_date)
+        self.assertEqual(post_text_0, PostURLTests.test_post_13.text)
+        self.assertEqual(post_pub_date_0, PostURLTests.test_post_13.pub_date)
         self.assertEqual(post_author_0, PostURLTests.user)
-        self.assertEqual(post_count_0 , PostURLTests.test_post.author.posts.count())
+        self.assertEqual(post_count_0 , PostURLTests.test_post_13.author.posts.count())
 
     def test_profile_page_show_correct_context(self):
         """Шаблон profile сформирован с правильным контекстом."""
@@ -181,10 +175,10 @@ class PostURLTests(TestCase):
         post_pub_date_0 = first_object.pub_date
         post_author_0 = first_object.author
         post_text_0 = first_object.text
-        self.assertEqual(post_text_0, PostURLTests.test_post12.text)
-        self.assertEqual(post_pub_date_0, PostURLTests.test_post12.pub_date)
+        self.assertEqual(post_text_0, PostURLTests.test_post_13.text)
+        self.assertEqual(post_pub_date_0, PostURLTests.test_post_13.pub_date)
         self.assertEqual(post_author_0, PostURLTests.user)
-        self.assertEqual(post_count_0, PostURLTests.test_post12.author.posts.count())
+        self.assertEqual(post_count_0, PostURLTests.test_post_13.author.posts.count())
   
     def test_first_index_page_contains_ten_records(self):
         '''Тест паджинатора, на главной странице 10 постов'''
@@ -212,14 +206,13 @@ class PostURLTests(TestCase):
 
     def test_first_profile_page_contains_ten_records(self):
         '''Тест паджинатора, на странице профиля 10 постов'''
-        response = self.client.get(reverse('posts:group_list', kwargs={'slug': PostURLTests.test_group.slug}))
+        response = self.client.get(reverse('posts:profile', kwargs={'username': PostURLTests.user}))
         # Проверка: количество постов на первой странице равно 10. 
         self.assertEqual(len(response.context['page_obj']), 10)
 
     def test_second_profile_page_contains_three_records(self):
         '''Тест паджинатора, на второй странице профиля 2 поста'''
         # Проверка: на второй странице должно быть три поста.
-        response = self.client.get(reverse('posts:group_list', kwargs={'slug': PostURLTests.test_group.slug}) + '?page=2')
-        self.assertEqual(len(response.context['page_obj']), 2)
-
+        response = self.client.get(reverse('posts:profile', kwargs={'username': PostURLTests.user}) + '?page=2')
+        self.assertEqual(len(response.context['page_obj']), 3)
 
