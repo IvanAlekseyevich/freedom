a66544c8e3f5314e4e82f212b1ed470907f9113b
pozarnik <pozarnik@tut.by>
2022-02-10 20:01:36 +0300

    Update test_urls.py

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -11,9 +11,8 @@ class PostURLTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        # Создадим запись в БД для проверки доступности адресов
         cls.user = User.objects.create_user(username='Test_author')
-        Group.objects.create(
+        cls.group = Group.objects.create(
             title='Тестовая группа',
             slug='testslug',
             description='Тестовое описание'
@@ -21,21 +20,18 @@ class PostURLTests(TestCase):
         cls.test_post = Post.objects.create(
             text='Тестовый заголовок',
             pub_date='1854-03-14',
-            author= cls.user
+            author= cls.user,
+            group=cls.group
+
         )
 
     def setUp(self):
-        # Создаем неавторизованный клиент
         self.guest_client = Client()
-        # Создаем пользователя
-        # self.user = User.objects.create_user(username='Test_user')
-        # Создаем второй клиент
         self.authorized_client = Client()
-        # Авторизуем пользователя
         self.authorized_client.force_login(PostURLTests.user)
 
-    def test_urls_available(self):
-        """Страницы доступны по данным URL-адресам для любого пользователя."""
+    def test_post_urls_available(self):
+        """Страницы приложения posts доступны по данным URL-адресам для любого пользователя."""
         PAGES = (
             '/',
             '/group/testslug/',
@@ -44,21 +40,20 @@ class PostURLTests(TestCase):
         )
         for page in PAGES:
             with self.subTest(f'{page=}'):
-                # response = self.guest_client.get(page)
                 self.assertTrue(HTTPStatus.OK)
 
     def test_post_create_url_exists_at_authorized_user(self):
-        """Страница /create/ доступна авторизованному пользователю."""
+        """Страница /create/ приложения posts доступна авторизованному пользователю."""
         response = self.authorized_client.get('/create/')
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
     def test_post_edit_url_exists_at_author(self):
-        """Страница /edit/ доступна автору поста."""
+        """Страница /edit/ приложения posts доступна автору поста."""
         response = self.authorized_client.post(f'/posts/{PostURLTests.test_post.id}/edit/')
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
     def test_post_url_redirect_anonymous(self):
-        """Страница /create/ перенаправляет анонимного пользователя."""
+        """Страница /create/ приложения posts перенаправляет анонимного пользователя."""
         response = self.guest_client.get('/create/')
         self.assertEqual(response.status_code, HTTPStatus.FOUND.value)
 
@@ -67,8 +62,8 @@ class PostURLTests(TestCase):
         response = self.guest_client.get('/enexisting_page/')
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND.value)
 
-    def test_urls_uses_correct_template(self):
-        """URL-адреса используют соответствующий шаблон."""
+    def test_post_urls_uses_correct_template(self):
+        """URL-адреса приложения posts используют соответствующий шаблон."""
         templates_url_names = {
             '/': 'posts/index.html',
             '/group/testslug/': 'posts/group_list.html',
@@ -81,4 +76,3 @@ class PostURLTests(TestCase):
             with self.subTest(url=url):
                 response = self.authorized_client.get(url)
                 self.assertTemplateUsed(response, template)
-
