3ad5c1ea132f6f0d2e3a660b2944eeac50cecc0f
pozarnik <pozarnik@tut.by>
2022-02-10 20:59:18 +0300

    Update test_urls.py

--- yatube/users/tests/test_urls.py
+++ yatube/users/tests/test_urls.py
@@ -5,19 +5,15 @@ from http import HTTPStatus
 User = get_user_model()
 
 
-class PostURLTests(TestCase):
+class UserURLTests(TestCase):
     def setUp(self):
-        # Создаем неавторизованный клиент
         self.guest_client = Client()
-        # Создаем пользователя
         self.user = User.objects.create_user(username='Test_user')
-        # Создаем второй клиент
         self.authorized_client = Client()
-        # Авторизуем пользователя
         self.authorized_client.force_login(self.user)
 
-    def test_urls_available(self):
-        """Страницы доступны по данным URL-адресам для любого пользователя."""
+    def test_user_urls_available(self):
+        '''Страницы приложения users доступны по данным URL-адресам для любого пользователя.'''
         PAGES = (
             '/auth/signup/',
             '/auth/login/',
@@ -25,21 +21,20 @@ class PostURLTests(TestCase):
         )
         for page in PAGES:
             with self.subTest(f'{page=}'):
-                # response = self.guest_client.get(page)
                 self.assertTrue(HTTPStatus.OK)
 
     def test_user_reset_password_url_exists_at_authorized_user(self):
-        """Страница /auth/password_change/ доступна авторизованному пользователю."""
+        '''Страница /auth/password_change/ приложения users доступна авторизованному пользователю.'''
         response = self.authorized_client.get('/auth/password_change/')
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
-    def test_url_redirect_anonymous(self):
-        """Страница /auth/password_change/ перенаправляет анонимного пользователя."""
+    def test_user_url_password_change_redirect_anonymous(self):
+        '''Страница /auth/password_change/ приложения users перенаправляет анонимного пользователя.'''
         response = self.guest_client.get('/auth/password_change/')
         self.assertEqual(response.status_code, HTTPStatus.FOUND.value)
 
-    def test_urls_user_correct_template(self):
-        """URL-адреса используют соответствующий шаблон."""
+    def test_user_urls_user_correct_template(self):
+        '''URL-адреса приложения users используют соответствующий шаблон.'''
         templates_url_names = {
             '/auth/signup/': 'users/signup.html',
             '/auth/logout/': 'users/logged_out.html',
@@ -54,8 +49,8 @@ class PostURLTests(TestCase):
                 response = self.authorized_client.get(url)
                 self.assertTemplateUsed(response, template)
 
-    def test_url_user_correct_template(self):
-        """URL-адрес использует соответствующий шаблон."""
+    def test_user_url_login_user_correct_template(self):
+        '''URL-адрес /login/ приложения users использует соответствующий шаблон.'''
         response = self.guest_client.get('/auth/login/')
         template = 'users/login.html'
         self.assertTemplateUsed(response, template)
