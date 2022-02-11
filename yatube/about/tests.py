3ace8a1a206e94799374affdc308514e6d98c774
pozarnik <pozarnik@tut.by>
2022-02-11 16:57:00 +0300

    Update tests.py

--- yatube/about/tests.py
+++ yatube/about/tests.py
@@ -14,16 +14,17 @@ class AboutURLTests(TestCase):
             '/about/tech/',
         )
         for page in PAGES:
-            with self.subTest(f'{page=}'):
-                self.assertTrue(HTTPStatus.OK)
+            with self.subTest(url=page):
+                response = self.guest_client.get(page)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_about_urls_uses_correct_template(self):
         '''URL-адреса страниц приложения about используют соответствующий шаблон.'''
         templates_url_names = {
-            'about/author.html': '/about/author/',
-            'about/tech.html': '/about/tech/',
+            '/about/author/': 'about/author.html',
+            '/about/tech/': 'about/tech.html',
         }
-        for template, url in templates_url_names.items():
+        for url, template in templates_url_names.items():
             with self.subTest(url=url):
                 response = self.guest_client.get(url)
                 self.assertTemplateUsed(response, template)
