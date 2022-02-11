505859b5aa9f681ba81e519df475663b409656f6
pozarnik <pozarnik@tut.by>
2022-02-11 22:51:01 +0300

    Визуальные изменения

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -13,16 +13,17 @@ class PostModelTest(TestCase):
         cls.user = User.objects.create_user(username='auth')
         cls.group = Group.objects.create(
             title='Название группы',
-            slug='Тестовый слаг',
+            slug='test_slug',
             description='Тестовое описание',
         )
         cls.post = Post.objects.create(
             author=cls.user,
             text='Тестовая группа',
+            group=cls.group,
         )
 
     def test_verbose_name_group(self):
-        '''verbose_name в полях группы совпадает с ожидаемым.'''
+        '''verbose_name в полях group совпадает с ожидаемым.'''
         task = PostModelTest.group
         field_verboses = {
             'title': 'Название группы',
@@ -34,8 +35,8 @@ class PostModelTest(TestCase):
                 self.assertEqual(
                     task._meta.get_field(field).verbose_name, expected_value)
 
-    def test_verbose_name_post_text(self):
-        '''verbose_name в полях поста совпадает с ожидаемым.'''
+    def test_verbose_name_post(self):
+        '''verbose_name в полях post совпадает с ожидаемым.'''
         task = PostModelTest.post
         field_verboses = {
             'text': 'Текст поста',
@@ -49,7 +50,7 @@ class PostModelTest(TestCase):
                     task._meta.get_field(field).verbose_name, expected_value)
 
     def test_help_text_post(self):
-        '''help_text в полях совпадает с ожидаемым.'''
+        '''help_text в полях post совпадает с ожидаемым.'''
         task = PostModelTest.post
         field_help_texts = {
             'text': 'Введите текст поста',
@@ -61,26 +62,25 @@ class PostModelTest(TestCase):
                     task._meta.get_field(field).help_text, expected_value) 
 
     def test_help_text_group(self):
-        '''help_text в полях совпадает с ожидаемым.'''
+        '''help_text в полях group совпадает с ожидаемым.'''
         task = PostModelTest.group
         field_help_texts = {
             'title': 'Введите название группы',
             'slug': ('Укажите уникальный адрес для страницы группы. Используйте только '
                      'латиницу, цифры, дефисы и знаки подчёркивания'),
-            'description': 'Введите название группы',              
-
+            'description': 'Введите название группы',
         }
         for field, expected_value in field_help_texts.items():
             with self.subTest(field=field):
                 self.assertEqual(
                     task._meta.get_field(field).help_text, expected_value) 
 
-    def test_models_have_correct_object_names_post(self):
+    def test_models_have_correct_object_text_post(self):
         '''Проверяем, что у модели post корректно работает __str__.'''
         post = PostModelTest.post
         self.assertEqual(post.text, str(post)) 
 
-    def test_models_have_correct_object_names_group(self):
+    def test_models_have_correct_object_title_group(self):
         '''Проверяем, что у модели group корректно работает __str__.'''
         group = PostModelTest.group
         self.assertEqual(group.title, str(group)) 
