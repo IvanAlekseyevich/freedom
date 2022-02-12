0584253634dab3e90dfaa679f7c645c01a860a7a
pozarnik <pozarnik@tut.by>
2022-02-12 06:36:36 +0300

    Update test_urls.py

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -20,7 +20,7 @@ class PostURLTests(TestCase):
         cls.test_post = Post.objects.create(
             text='Тестовый заголовок',
             pub_date='1854-03-14',
-            author= cls.user,
+            author=cls.user,
             group=cls.group
         )
 
@@ -33,52 +33,52 @@ class PostURLTests(TestCase):
         self.authorized_author.force_login(PostURLTests.user)
 
     def test_post_urls_unauth_user_available(self):
-        '''Страницы приложения posts доступны по данным URL-адресам для всех пользователей.'''
-        PAGES = (
+        """Страницы приложения posts доступны по данным URL-адресам для всех пользователей."""
+        pages = (
             '/',
             '/group/testslug/',
             '/profile/Test_author/',
             f'/posts/{PostURLTests.test_post.id}/',
         )
-        for page in PAGES:
+        for page in pages:
             with self.subTest(url=page):
                 response = self.guest_client.get(page)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_post_create_url_exists_at_authorized_user(self):
-        '''Страница /create/ приложения posts доступна авторизованному пользователю.'''
+        """Страница /create/ приложения posts доступна авторизованному пользователю."""
         response = self.authorized_client.get('/create/')
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
     def test_post_url_redirect_anonymous(self):
-        '''Страница /create/ приложения posts перенаправляет анонимного пользователя.'''
+        """Страница /create/ приложения posts перенаправляет анонимного пользователя."""
         response = self.guest_client.get('/create/')
         self.assertEqual(response.status_code, HTTPStatus.FOUND.value)
 
     def test_post_edit_url_exists_at_author(self):
-        '''Страница /edit/ приложения posts доступна автору поста.'''
+        """Страница /edit/ приложения posts доступна автору поста."""
         response = self.authorized_author.post(f'/posts/{PostURLTests.test_post.id}/edit/')
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
     def test_post_edit_url_redirect_no_author(self):
-        '''Страница /edit/ приложения posts перенаправляет не автора поста.'''
+        """Страница /edit/ приложения posts перенаправляет не автора поста."""
         response = self.authorized_client.post(f'/posts/{PostURLTests.test_post.id}/edit/')
-        self.assertEqual(response.status_code, HTTPStatus.FOUND.value)    
+        self.assertEqual(response.status_code, HTTPStatus.FOUND.value)
 
     def test_enexisting_page_url_redirect(self):
-        '''Несуществующая страница /enexisting_page/ вернет ошибку 404.'''
+        """Несуществующая страница /enexisting_page/ вернет ошибку 404."""
         response = self.guest_client.get('/enexisting_page/')
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND.value)
 
     def test_post_urls_uses_correct_template(self):
-        '''URL-адреса приложения posts используют соответствующий шаблон.'''
+        """URL-адреса приложения posts используют соответствующий шаблон."""
         templates_url_names = {
             '/': 'posts/index.html',
             '/group/testslug/': 'posts/group_list.html',
             '/profile/Test_author/': 'posts/profile.html',
             f'/posts/{PostURLTests.test_post.id}/': 'posts/post_detail.html',
             f'/posts/{PostURLTests.test_post.id}/edit/': 'posts/create_post.html',
-            '/create/': 'posts/create_post.html', 
+            '/create/': 'posts/create_post.html',
         }
         for url, template in templates_url_names.items():
             with self.subTest(url=url):
