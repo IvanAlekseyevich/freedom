6c8b42a94c91863c04c1321702b6fee3369152be
pozarnik <pozarnik@tut.by>
2022-02-12 06:38:11 +0300

    Update test_views.py

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 from posts.models import Post, Group
 
-
 User = get_user_model()
 
 
@@ -24,11 +23,11 @@ class PostURLTests(TestCase):
         )
         for i in range(12):
             Post.objects.create(
-            text='Тестовый пост',
-            pub_date='1854-03-14',
-            author= cls.user,
-            group = cls.test_group
-        )
+                text='Тестовый пост',
+                pub_date='1854-03-14',
+                author=cls.user,
+                group=cls.test_group
+            )
         cls.test_post = Post.objects.create(
             text='Тестовый пост',
             pub_date=cls.date,
@@ -52,7 +51,7 @@ class PostURLTests(TestCase):
             reverse('posts:profile', args=(PostURLTests.user,)): 'posts/profile.html',
             reverse('posts:post_detail', args=(PostURLTests.test_post.id,)): 'posts/post_detail.html',
             reverse('posts:post_edit', args=(PostURLTests.test_post.id,)): 'posts/create_post.html',
-            reverse('posts:post_create'): 'posts/create_post.html', 
+            reverse('posts:post_create'): 'posts/create_post.html',
         }
         for reverse_name, template in templates_url_names.items():
             with self.subTest(reverse_name=reverse_name):
@@ -112,7 +111,7 @@ class PostURLTests(TestCase):
         self.assertEqual(post_text_0, PostURLTests.test_post.text)
         self.assertEqual(post_pub_date_0, PostURLTests.test_post.pub_date)
         self.assertEqual(post_author_0, PostURLTests.user)
-        self.assertEqual(post_count_0 , PostURLTests.test_post.author.posts.count())
+        self.assertEqual(post_count_0, PostURLTests.test_post.author.posts.count())
 
     def test_post_create_post_page_show_correct_context(self):
         """Шаблон создания поста приложения posts сформирован с правильным контекстом."""
@@ -122,52 +121,52 @@ class PostURLTests(TestCase):
             # При создании формы поля модели типа TextField 
             # преобразуются в CharField с виджетом forms.Textarea           
             'group': forms.fields.ChoiceField,
-        }        
+        }
         for value, expected in form_fields.items():
             with self.subTest(value=value):
                 form_field = response.context.get('form').fields.get(value)
                 self.assertIsInstance(form_field, expected)
-            
+
     def test_post_edit_post_page_show_correct_context(self):
         """Шаблон редактирования поста приложения posts сформирован с правильным контекстом."""
         response = self.authorized_client.get(reverse('posts:post_edit', args=(PostURLTests.test_post.id,)))
         form_fields = {
-            'text': forms.fields.CharField,         
+            'text': forms.fields.CharField,
             'group': forms.fields.ChoiceField,
-        }        
+        }
         for value, expected in form_fields.items():
             with self.subTest(value=value):
                 form_field = response.context.get('form').fields.get(value)
                 self.assertIsInstance(form_field, expected)
-  
+
     def test_post_first_index_page_contains_ten_records(self):
-        '''Тест паджинатора страницы index, на главной странице 10 постов'''
+        """Тест паджинатора страницы index, на главной странице 10 постов"""
         response = self.client.get(reverse('posts:index'))
         # Проверка: количество постов на первой странице равно 10. 
         self.assertEqual(len(response.context['page_obj']), 10)
 
     def test_post_second_index_page_contains_three_records(self):
-        '''Тест паджинатора страницы index, на второй странице 3 поста'''
+        """Тест паджинатора страницы index, на второй странице 3 поста"""
         # Проверка: на второй странице должно быть три поста.
         response = self.client.get(reverse('posts:index') + '?page=2')
         self.assertEqual(len(response.context['page_obj']), 3)
 
     def test_post_first_group_page_contains_ten_records(self):
-        '''Тест паджинатора, на странице группы 10 постов'''
+        """Тест паджинатора, на странице группы 10 постов"""
         response = self.client.get(reverse('posts:group_list', args=(PostURLTests.test_group.slug,)))
         self.assertEqual(len(response.context['page_obj']), 10)
 
     def test_post_second_group_page_contains_three_records(self):
-        '''Тест паджинатора, на второй странице группы 2 поста'''
+        """Тест паджинатора, на второй странице группы 2 поста"""
         response = self.client.get(reverse('posts:group_list', args=(PostURLTests.test_group.slug,)) + '?page=2')
         self.assertEqual(len(response.context['page_obj']), 2)
 
     def test_post_first_profile_page_contains_ten_records(self):
-        '''Тест паджинатора, на странице профиля 10 постов'''
+        """Тест паджинатора, на странице профиля 10 постов"""
         response = self.client.get(reverse('posts:profile', args=(PostURLTests.user,)))
         self.assertEqual(len(response.context['page_obj']), 10)
 
     def test_post_second_profile_page_contains_three_records(self):
-        '''Тест паджинатора, на второй странице профиля 3 поста'''
+        """Тест паджинатора, на второй странице профиля 3 поста"""
         response = self.client.get(reverse('posts:profile', args=(PostURLTests.user,)) + '?page=2')
         self.assertEqual(len(response.context['page_obj']), 3)
