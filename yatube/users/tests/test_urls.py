235b20f4c9f416d969240c184f4999880299ed4e
pozarnik <pozarnik@tut.by>
2022-02-14 02:00:55 +0300

    Update test

--- yatube/users/tests/test_urls.py
+++ yatube/users/tests/test_urls.py
@@ -7,52 +7,67 @@ User = get_user_model()
 
 
 class UserURLTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.signup_url = '/auth/signup/'
+        cls.login_url = '/auth/login/'
+        cls.logout_url = '/auth/logout/'
+        cls.password_change_url = '/auth/password_change/'
+        cls.password_change_done_url = '/auth/password_change/done/'
+        cls.password_reset_url = '/auth/password_reset/'
+        cls.password_reset_done_url = '/auth/password_reset/done/'
+        cls.reset_done_url = '/auth/reset/done/'
+
     def setUp(self):
         self.guest_client = Client()
-        self.user = User.objects.create_user(username='Test_user')
         self.authorized_client = Client()
+        self.user = User.objects.create_user(username='Test_user')
         self.authorized_client.force_login(self.user)
 
     def test_user_urls_available(self):
-        '''Страницы приложения users доступны по данным URL-адресам для любого пользователя.'''
-        PAGES = (
-            '/auth/signup/',
-            '/auth/login/',
-            '/auth/password_reset/',
-        )
-        for page in PAGES:
+        """Страницы приложения users доступны по данным URL-адресам для любого пользователя."""
+        pages = [
+            UserURLTests.signup_url,
+            UserURLTests.login_url,
+            UserURLTests.password_reset_url,
+        ]
+        for page in pages:
             with self.subTest(url=page):
                 response = self.guest_client.get(page)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_user_reset_password_url_exists_at_authorized_user(self):
-        '''Страница /auth/password_change/ приложения users доступна авторизованному пользователю.'''
-        response = self.authorized_client.get('/auth/password_change/')
-        self.assertEqual(response.status_code, HTTPStatus.OK.value)
+        """Страница /auth/password_change/ приложения users доступна авторизованному пользователю."""
+        response = self.authorized_client.get(UserURLTests.password_change_url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_user_url_password_change_redirect_anonymous(self):
-        '''Страница /auth/password_change/ приложения users перенаправляет анонимного пользователя.'''
-        response = self.guest_client.get('/auth/password_change/')
-        self.assertEqual(response.status_code, HTTPStatus.FOUND.value)
+        """Страница /auth/password_change/ приложения users перенаправляет анонимного пользователя."""
+        response = self.guest_client.get(UserURLTests.password_change_url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
 
     def test_user_urls_user_correct_template(self):
-        '''URL-адреса приложения users используют соответствующий шаблон.'''
+        """URL-адреса приложения users используют соответствующий шаблон."""
         templates_url_names = {
-            '/auth/signup/': 'users/signup.html',
-            '/auth/logout/': 'users/logged_out.html',
-            # '/auth/password_change/': 'users/password_change_form.html',  #Не работает
-            # '/auth/password_change/done/': 'users/password_change_done.html',  #Не работает
-            '/auth/password_reset/': 'users/password_reset_form.html',
-            '/auth/password_reset/done/': 'users/password_reset_done.html',
-            '/auth/reset/done/': 'users/password_reset_complete.html',
+            UserURLTests.signup_url: 'users/signup.html',
+            UserURLTests.password_change_url: 'users/password_change_form.html',
+            UserURLTests.password_change_done_url: 'users/password_change_done.html',
+            UserURLTests.password_reset_url: 'users/password_reset_form.html',
+            UserURLTests.password_reset_done_url: 'users/password_reset_done.html',
+            UserURLTests.reset_done_url: 'users/password_reset_complete.html',
+            UserURLTests.logout_url: 'users/logged_out.html',
+            UserURLTests.login_url: 'users/login.html'
         }
         for url, template in templates_url_names.items():
             with self.subTest(url=url):
                 response = self.authorized_client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
                 self.assertTemplateUsed(response, template)
 
     def test_user_url_login_user_correct_template(self):
-        '''URL-адрес /login/ приложения users использует соответствующий шаблон.'''
-        response = self.guest_client.get('/auth/login/')
+        """URL-адрес /login/ приложения users использует соответствующий шаблон."""
+        response = self.guest_client.get(UserURLTests.login_url)
         template = 'users/login.html'
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, template)
