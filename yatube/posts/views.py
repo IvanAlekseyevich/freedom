06265c28d1ca2bb6e9686a52985ece07f3977980
pozarnik <pozarnik@tut.by>
2022-02-16 15:26:18 +0300

    Add cache for index page

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, render, get_object_or_404
+from django.views.decorators.cache import cache_page
 
 from .forms import PostForm, CommentForm
 from .models import Post, Group, User
 
 
+@cache_page(20)
 def index(request):
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 10)
