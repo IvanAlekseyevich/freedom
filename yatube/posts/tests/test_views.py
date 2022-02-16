ff0720acf0fe0602efa21eaa92eb08cade3c5e76
pozarnik <pozarnik@tut.by>
2022-02-16 16:22:32 +0300

    Update test

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,7 +1,9 @@
 import shutil
 import tempfile
 from datetime import datetime
+from http import HTTPStatus
 
+from django import forms
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
