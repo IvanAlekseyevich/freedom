4f3b253d2bf4d57146a4aa8a131f5ac99c3f9735
pozarnik <pozarnik@tut.by>
2022-02-16 15:25:54 +0300

    Add cache

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -136,3 +136,9 @@ CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
